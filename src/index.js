8111c41ee05a44b3f4e3bbe060f6ae5fa13ee806
Szabó Ákos <szabo.akos2000@gmail.com>
2021-12-01 10:58:03 +0100

    2021.12.01.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { playlistsStorage } from './api/PlaylistsStorage';
-import { tracksStorage } from './api/TrackStorage';
-import { App } from './App';
-import { BlackAndWhite } from './BlackAndWhite';
-import { examplePlaylists } from './domain/playlist';
-import { exampleTracks } from './domain/track';
-import { Game } from './Game';
-import { PlaylistsProvider } from './views/state/PlaylistsService';
-import { TracksProvider } from './views/state/TracksService';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { playlistsStorage } from "./api/PlaylistsStorage";
+import { tracksStorage } from "./api/TrackStorage";
+import { App } from "./App";
+import { BlackAndWhite } from "./BlackAndWhite";
+import { examplePlaylists } from "./domain/playlist";
+import { exampleTracks } from "./domain/track";
+import { Game } from "./Game";
+import { PlaylistsProvider } from "./views/state/PlaylistsService";
+import { TracksProvider } from "./views/state/TracksService";
+
+const initialState = {
+  playlists: examplePlaylists,
+  tracks: exampleTracks,
+};
+
+const rootReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+  if (type === "ADD_TRACK") {
+    return {
+      tracks: [...state.tracks, payload],
+      playlists: state.playlists,
+    };
+  }
+  return state;
+};
+
+const store = createStore(rootReducer);
+console.log(store.getState());
+store.dispatch({
+  type: "ADD_TRACK",
+  payload: { title: "q", artist: "w" },
+});
+console.log(store.getState());
 
 const render = () =>
   ReactDOM.render(
@@ -17,13 +42,13 @@ const render = () =>
         <App />
       </PlaylistsProvider>
     </TracksProvider>,
-    document.getElementById('root')
+    document.getElementById("root")
   );
 
 const start = async () => {
-  const newPlaylists = await playlistsStorage.fill(examplePlaylists)
-  const newTracks = await tracksStorage.fill(exampleTracks) 
+  const newPlaylists = await playlistsStorage.fill(examplePlaylists);
+  const newTracks = await tracksStorage.fill(exampleTracks);
 
-  render()
-}
-start()
+  render();
+};
+start();
