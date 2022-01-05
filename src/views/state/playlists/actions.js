f0f5fd1274b1331e59b0c38a859c4334236b8ea0
Szabó Ákos <szabo.akos2000@gmail.com>
2022-01-05 10:59:49 +0100

    2022.01.05.

--- src/views/state/playlists/actions.js
+++ src/views/state/playlists/actions.js
@@ -1,18 +1,57 @@
+import { playlistsStorage } from "../../../api/PlaylistsStorage";
+import { getPlaylists } from "./selectors";
+
 export const SET_PLAYLISTS = "SET_PLAYLISTS";
 export const ADD_PLAYLIST = "ADD_PLAYLIST";
 export const ADD_TRACK_TO_PLAYLIST = "ADD_TRACK_TO_PLAYLIST";
+export const UPDATE_PLAYLIST = "UPDATE_PLAYLIST";
 
+//Sync
 export const setPlaylists = (playlists) => ({
   type: SET_PLAYLISTS,
   payload: playlists,
 });
 
-export const addPlaylist = (title) => ({
+export const addPlaylistToStore = (title) => ({
   type: ADD_PLAYLIST,
   payload: { id: Date.now().toString(), title, tracks: [] },
 });
 
-export const addTrackToPlaylist = (playlistId, trackId) => ({
+export const updatePlaylist = (playlist) => ({
+  type: UPDATE_PLAYLIST,
+  payload: playlist,
+});
+
+export const addTrackToPlaylistToStore = (playlistId, trackId) => ({
   type: ADD_TRACK_TO_PLAYLIST,
   payload: { playlistId, trackId },
 });
+
+//Async
+export const fetchPlaylists = () => async (dispatch) => {
+  const playlists = await playlistsStorage.getAll();
+  dispatch(setPlaylists(playlists));
+};
+
+export const addPlaylist = (title) => async (dispatch) => {
+  const newPlaylist = await playlistsStorage.create({ title, tracks: [] });
+  dispatch(addPlaylistToStore(newPlaylist));
+};
+
+export const addTrackToPlaylist =
+  (playlistId, trackId) => async (dispatch, getState) => {
+    const playlists = getPlaylists(getState());
+    const playlist = playlists.find((pl) => pl.id === playlistId);
+
+    if (!playlist) return;
+
+    if (playlist.tracks.includes(trackId)) return;
+
+    const modifiedPlaylist = {
+      ...playlist,
+      tracks: playlist.tracks.concat(trackId),
+    };
+
+    const updatedPlaylist = await playlistsStorage.update(modifiedPlaylist);
+    dispatch(updatedPlaylist(updatedPlaylist));
+  };
